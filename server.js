6fc2ffb81f9be834b84f3646a865bba4d01c7829
bubba <boubasambare@yahoo.it>
2020-10-04 12:17:29 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import expressListEndpoints from 'express-list-endpoints';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+mongoose.connect(
+process.env.MONGO_URI
+,{
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify:true
+  
+},() => console.log('db connected'));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 4000;
+
+app.get('/test', (req, res) => {
+    res.status(200).send('working');
+});
+
+
+console.table(expressListEndpoints(app));
+
+app.listen(PORT, () => {
+    console.log(`Server started on ${PORT}`);
+});
